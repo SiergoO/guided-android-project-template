056493bdce3dd0db6dcefc6196c1b1a3447adad1
SiergoO@github.com <soulflavacrew96@gmail.com>
2024-01-06 15:35:46 +0200

    Implement BottomNavigationBar and BottomNavigationBarItem composable components.

---
+++ core/ui/src/main/kotlin/com/template/cleanlibrary/core/ui/components/BottomNavigationBar.kt
@@ -0,0 +1,26 @@
+package com.template.cleanlibrary.core.ui.components
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BottomNavigationBar(
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.() -> Unit,
+) {
+
+    NavigationBar(
+        modifier = modifier
+            .navigationBarsPadding()
+            .height(64.dp),
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.tertiary,
+        content = content
+    )
+}
---
+++ core/ui/src/main/kotlin/com/template/cleanlibrary/core/ui/components/BottomNavigationBarItem.kt
@@ -0,0 +1,37 @@
+package com.template.cleanlibrary.core.ui.components
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RowScope.BottomNavigationBarItem(
+    modifier: Modifier = Modifier,
+    selected: Boolean,
+    onClick: () -> Unit,
+    icon: @Composable () -> Unit,
+    selectedIcon: @Composable () -> Unit = icon,
+    enabled: Boolean = true,
+    label: @Composable (() -> Unit)? = null,
+    alwaysShowLabel: Boolean = true,
+) {
+    NavigationBarItem(
+        modifier = modifier,
+        selected = selected,
+        onClick = onClick,
+        icon = if (selected) selectedIcon else icon,
+        enabled = enabled,
+        label = label,
+        alwaysShowLabel = true,
+        colors = NavigationBarItemDefaults.colors(
+            selectedIconColor = MaterialTheme.colorScheme.background,
+            selectedTextColor = MaterialTheme.colorScheme.background,
+            indicatorColor = MaterialTheme.colorScheme.tertiary,
+            unselectedIconColor = MaterialTheme.colorScheme.background.copy(alpha = 0.6f),
+            unselectedTextColor = MaterialTheme.colorScheme.background.copy(alpha = 0.6f),
+        ),
+    )
+}
